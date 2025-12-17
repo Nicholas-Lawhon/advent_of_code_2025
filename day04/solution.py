8dcf35cc9fbb5b1426d7c8093c649d730d62309b
Nicholas Lawhon <nicholaslawhon09@gmail.com>
2025-12-16 22:34:21 -0600

    Setup Day04 project structure

---
+++ day04/solution.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+
+INPUT_FILE = Path(__file__).with_name("input.txt")
+
+
+def parse_input():
+    pass
+
+
+
+def part01(data):
+    pass
+
+
+
+def part02(data):
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
