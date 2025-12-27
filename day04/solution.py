b013812604963ecc07fbf94e72c133e094b25104
Nicholas Lawhon <nick@nicholaslawhon.com>
2025-12-27 11:29:19 -0600

    Setup parse function to create a grid for traversal.

--- day04/solution.py
+++ day04/solution.py
@@ -5,12 +5,29 @@ INPUT_FILE = Path(__file__).with_name("input.txt")
 
 
 def parse_input():
-    pass
+    grid: list[list[str]] = []
+    raw_data = INPUT_FILE.read_text().strip().splitlines()
+
+    for line in raw_data:
+        if not line:
+            continue
+
+        row = list(line)
+        grid.append(row)
+
+    return grid
 
 
 
 def part01(data):
-    pass
+    directions = [
+    (-1, -1), (-1, 0), (-1, 1),   # Top-Left, Top, Top-Right
+    (0, -1),           (0, 1),    # Left,        Right
+    (1, -1),  (1, 0),  (1, 1)     # Bottom-Left, Bottom, Bottom-Right
+]
+    
+    for i in data:
+        print(i)
 
 
 
@@ -20,4 +37,6 @@ def part02(data):
 
 
 if __name__ == "__main__":
-    pass
+    data = parse_input()
+
+    print("Part 01:", part01(data))
